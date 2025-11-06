44a67283f82ddc053461d6c1b8c25e113084e372
dbwltnwl <jisoo9305@gmail.com>
2025-11-07 03:18:30 +0900

    Update hero.tsx

--- components/hero.tsx
+++ components/hero.tsx
@@ -37,8 +37,8 @@ export function Hero() {
   
   const defaultInfo = {
     greeting: "안녕하세요,",
-    name: "정재훈",
-    title: "단국대 교수입니다",
+    name: "유지수",
+    title: "단국대 학생입니다",
     description: "프롭테크 기술로 부동산 시장을 분석합니다.",
     profileImage: "/uploads/hero-profile-1761477237286.png",
     backgroundImage: "",
